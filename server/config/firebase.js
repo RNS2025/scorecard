5740e5611982768c94a506e228d132c5bba3b123
Luux2 <293428@via.dk>
2026-03-31 12:02:40 +0200

    fix

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -3,7 +3,15 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require(path.resolve(__dirname, '..', process.env.FIREBASE_SERVICE_ACCOUNT));
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+    // Production (Render): credentials from environment variable
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+} else {
+    // Local dev: credentials from file
+    serviceAccount = require(path.resolve(__dirname, '..', process.env.FIREBASE_SERVICE_ACCOUNT));
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
